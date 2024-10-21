5349dd11f59c79db3c856263ec388a6b25871eb3
saralaloux <89387448+saralaloux@users.noreply.github.com>
2024-10-21 19:11:23 +0200

    Add files via upload
    modif mobile & button

--- script.js
+++ script.js
@@ -57,3 +57,13 @@ function initItyped(element, options) {
 document.getElementById("resumeButton").addEventListener("click", function() {
     window.open("./src/assets/laloux-cv-ux-2024.pdf", "_blank");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerMenu = document.querySelector("#burger-menu");
+    const menu = document.querySelector("#menu");
+
+    burgerMenu.addEventListener("click", () => {
+        burgerMenu.classList.toggle("close");
+        menu.classList.toggle("overlay");
+    });
+});
